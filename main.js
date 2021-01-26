cbcf7ccee241e101143febb8ad492f680c6ff4f9
Lensman <anministry.mgmt@gmail.com>
2021-01-26 20:03:14 +0200

    Added dictionary and cases to test.

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-let input =
+let input1 =
   'Мне не удобно. Перенесите пожалуйста запись на субботу или 26(ое) 27(ое) с 14(и)00(и) до 16(и)15(и).';
+let input2 =
+  'Мне не удобно. Перенесите пожалуйста запись на следующую неделю, среда или четверг с 14(и)00 до 16(и)15(и)';
+let input3 =
+  'Мне не удобно. Перенесите пожалуйста запись на 15(ое) февраля с 14(и)00(и) до 16(и)15(и)';
+let input4 =
+  'Мне не удобно. Перенесите пожалуйста запись на среду с 12(и)15(и) до 15(и)30(и) или пятницу с 19(и)25(и) до 20(и)45(и)';
 let parsedInput = [];
 
-parsedInput = input.toLowerCase().split(' ');
+parsedInput = input4.toLowerCase().split(' ');
 
 function f(x) {
   if (
@@ -13,7 +19,22 @@ function f(x) {
     x.includes('четверг') ||
     x.includes('пятниц') ||
     x.includes('суббот') ||
-    x.includes('воскресен')
+    x.includes('воскресен') ||
+    x.includes('следующ') ||
+    x.includes('недел') ||
+    x.includes('январ') ||
+    x.includes('феврал') ||
+    x.includes('март') ||
+    x.includes('апрел') ||
+    x.includes('мая') ||
+    x.includes('май') ||
+    x.includes('июн') ||
+    x.includes('июл') ||
+    x.includes('август') ||
+    x.includes('сентябр') ||
+    x.includes('октябр') ||
+    x.includes('ноябр') ||
+    x.includes('декабр')
   ) {
     return x;
   } else if (x.includes('(ое)')) {
